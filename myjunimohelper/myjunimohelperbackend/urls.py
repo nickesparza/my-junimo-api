90508fd351c6cbdbe425b218718369700f424dae
Alys <alice.g.cooper@gmail.com>
2022-08-22 10:46:28 -0400

    JSON at /characters returning empty as intended

--- myjunimohelper/myjunimohelperbackend/urls.py
+++ myjunimohelper/myjunimohelperbackend/urls.py
@@ -1,7 +1,10 @@
 # first_app/urls.py
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('characters/', include('junimoDatabase.urls')),
     path('', views.index, name='index'),
 ]
